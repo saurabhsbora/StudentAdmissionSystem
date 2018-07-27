83e348c8351fc69c9ad3c291a1068382af55f218
enthussb <saurabhbora@gmail.com>
2018-07-27 23:54:57 +0530

    Removed Redundant Code

--- Student Admission System/src/com/sdl/StudentAdmission/Admin.java
+++ Student Admission System/src/com/sdl/StudentAdmission/Admin.java
@@ -56,43 +56,4 @@ public class Admin
 		}while(ch!=5);
 		sc.close();
 	}
-	public static void main(String args[]) throws IOException, ClassNotFoundException, SQLException
-	{
-		int choice;
-		String start = "---STUDENT ADMISSION SYSTEM---";
-		String menu = "\n1.Register as a Student\n2.Login as Admin\n3.Exit\nEnter your choice:";
-		
-		ServerSocket Serversoc = new ServerSocket(5000);
-		System.out.println("Waiting for connection...");
-		Socket soc = Serversoc.accept();
-		System.out.println("Client Connected "+soc.getInetAddress());
-		DatabaseConnection m = new DatabaseConnection();
-		
-		DataOutputStream dout = new DataOutputStream(soc.getOutputStream());
-        DataInputStream din = new DataInputStream(soc.getInputStream());
-        dout.writeUTF(start);
-        do 
-        {
-        	//Displaying menu on client side
-        	dout.writeUTF(menu);
-        	choice = din.readInt();
-        	switch(choice)
-        	{
-        		case 1:ObjectInputStream ois = new ObjectInputStream(soc.getInputStream());
-            		   Student stud = (Student)ois.readObject();
-            		   Global.obj.add(stud);
-            		   dout.writeUTF("Successfully Registered");
-            		   m.writeTomysql(stud);
-            		   break;
-        		case 2:System.out.println("Logged as Admin");
-            		   //System.out.println("Count:"+m.count());	
-        				menu(m);
-            		   break;
-        	}
-        }while(choice!=3);
-        System.out.println("Client Disconnected");
-        dout.close();
-        din.close();
-        Serversoc.close();
-	}
 }
--- Student Admission System/src/com/sdl/StudentAdmission/Student.java
+++ Student Admission System/src/com/sdl/StudentAdmission/Student.java
@@ -1,11 +1,5 @@
 package com.sdl.StudentAdmission;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Objects;
 import java.util.Scanner;
 public class Student implements Comparable<Student>,Serializable
@@ -136,37 +130,5 @@ public class Student implements Comparable<Student>,Serializable
 	public int hashCode() 
 	{
 	      return Objects.hash(id);
-	}
-	public static void main(String args[]) throws UnknownHostException, IOException, ClassNotFoundException
-	{
-			int ch;
-			Socket clientsoc = new Socket("localhost",5000);
-			
-			DataOutputStream dout = new DataOutputStream(clientsoc.getOutputStream());
-	        DataInputStream din = new DataInputStream(clientsoc.getInputStream());
-	        Scanner sc1 = new Scanner(System.in);
-	        System.out.print(din.readUTF());
-	        do 
-			{
-				System.out.print(din.readUTF());
-				ch = sc1.nextInt();
-				dout.writeInt(ch);
-				switch(ch)
-				{
-				case 1:	Student s = new Student();
-						s.register();
-						ObjectOutputStream oos = new ObjectOutputStream(clientsoc.getOutputStream());
-						oos.writeObject(s);
-						System.out.print(din.readUTF());
-						break;
-				case 2:System.out.println("In admin Window");
-						break;
-				}
-			}while(ch!=3);
-	       System.out.println("GoodBye :)");
-	       sc1.close();
-	       dout.close();
-	       din.close();
-	       clientsoc.close();
 	}
 }
