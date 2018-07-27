e91d6973265664687406b0968bbcc129b1ea0f0a
enthussb <saurabhbora@gmail.com>
2018-07-27 23:12:26 +0530

    Global DataStructures changes

--- Student Admission System/src/com/sdl/StudentAdmission/Global.java
+++ Student Admission System/src/com/sdl/StudentAdmission/Global.java
@@ -1,7 +1,11 @@
 package com.sdl.StudentAdmission;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.HashMap;
 public class Global 
 {
 	public static Set<Student> obj = new TreeSet<Student>();
+	//public static HashMap<String, Integer> COMP = new HashMap<>();
+	//public static HashMap<String, Integer> IT = new HashMap<>();
+	//public static HashMap<String, Integer> ENTC = new HashMap<>();
 }
